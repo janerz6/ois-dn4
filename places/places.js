54d21034b4b8b579edde24d7befe9bc9ed480df0
janerz6 <je1468@student.uni-lj.si>
2014-12-06 21:40:30 +0000

    Priporocena mesta za uporabnika + bootstrap

--- places/places.js
+++ places/places.js
@@ -84,7 +84,7 @@ function createMarkers(places) {
       position: place.geometry.location
     });
 
-    placesList.innerHTML += '<a href="#" value="'+place.place_id+' onclick="selectMarker(this)" class="list-group-item">'+ place.name+'</a>';
+    placesList.innerHTML += '<a href="#" value="'+place.place_id+' class="list-group-item">'+ place.name+'</a>';
     
     
     google.maps.event.addListener(marker, 'click', function(){
@@ -137,13 +137,12 @@ function createMarkers(places) {
   
   
 }
-function selectmarker(th){
-   alert("delej");
-   console.log("Delej");
-}
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
 $(document).ready(function(){
- 
+ $('.places').on('click', 'a', function() {
+   alert("Delej");
+});
   
 });
