adb24ebb7c349aec5d63519d22dbb5fd077adfec
janerz6 <je1468@student.uni-lj.si>
2014-12-07 18:55:23 +0000

    Priporocila

--- places/places.js
+++ places/places.js
@@ -154,7 +154,9 @@ function getURLParameter(name)
         var param = params[i].split('=');
         if (param[0] == name) 
         {
-            return param[1];
+          var val = param[1];
+          val.replace('%27',"'");
+          return val;
         }
     }
 }
