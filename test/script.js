e9e334d151a620bd5464efd0f0c4075396764b49
janerz6 <je1468@student.uni-lj.si>
2014-12-12 20:59:34 +0000

    Popravljen slog

--- test/script.js
+++ test/script.js
@@ -401,7 +401,7 @@ function dodajVitalne(){
 $(document).ready(function() {
 	$(window).on('resize',function(){
 		w = $('#chartWrapper').width();
-		h = $('#chartWrapper').height();
+		h = $('#status_vitals').height();
 		draw();
 	});
 	$('#patients').change(function() {
@@ -413,7 +413,7 @@ $(document).ready(function() {
 			query($(this).val());
 			$('.personalInfo').slideDown(1500);
 			w = $('#chartWrapper').width();
-			h = $('#chartWrapper').height();
+			h = $('#status_vitals').height();
 			draw();
 		}
 	});
